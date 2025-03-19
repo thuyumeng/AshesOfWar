317eeedc35a7dc45b3d112167436bf972c5e675b
于蒙 <gzyumeng201207@corp.netease.com>
2025-03-19 21:32:13 +0800

    refs #week_1 add a new unit c++ class

--- Source/AshesOfWar/AshesOfWar.Build.cs
+++ Source/AshesOfWar/AshesOfWar.Build.cs
@@ -8,7 +8,17 @@ public class AshesOfWar : ModuleRules
     {
         PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[]
+		{
+			"Core", 
+			"CoreUObject", 
+			"Engine", 
+			"InputCore", 
+			"EnhancedInput", 
+			"GameplayAbilities",
+			"GameplayTags", 
+			"GameplayTasks"
+		});
 
         PublicDependencyModuleNames.AddRange(new string[] 
         { 
