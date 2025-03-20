284441d4fd44e17022052ac67e4cd560d4a6beef
于蒙 <gzyumeng201207@corp.netease.com>
2025-03-20 14:49:13 +0800

    refs #week_1 add ability system component

---
+++ Source/AshesOfWar/AbilitySystem/AOWAbilitySystemComponent.h
@@ -0,0 +1,26 @@
+// This file is the ability system component for the game. It is used to manage the abilities of the game characters.
+
+#pragma once
+
+#include "AOWAbilitySystemComponent.generated.h"
+#include "AbilitySystemComponent.h"
+#include "CoreMinimal.h"
+
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
+class ASHESOFWAR_API UAOWAbilitySystemComponent : public UAbilitySystemComponent {
+  GENERATED_BODY()
+
+public:
+  // Sets default values for this component's properties
+  UAOWAbilitySystemComponent();
+
+protected:
+  // Called when the game starts
+  virtual void BeginPlay() override;
+
+public:
+  // Called every frame
+  virtual void
+  TickComponent(float DeltaTime, ELevelTick TickType,
+                FActorComponentTickFunction *ThisTickFunction) override;
+};
