e76413b309670a639e6244b9e47f8f0f9aaba785
yumeng <yumengch727727@gmail.com>
2025-04-04 19:05:48 +0800

    refs #week_3 select the unit and move to location

---
+++ Source/AshesOfWar/Units/Interface/AOWInputInterface.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "AOWInputInterface.generated.h"
+
+// This class does not need to be modified.
+UINTERFACE()
+class UAOWInputInterface : public UInterface
+{
+	GENERATED_BODY()
+};
+
+/**
+ * this class is used to define the input interface for the unit
+ */
+
+class AUnit;
+class ASHESOFWAR_API IAOWInputInterface
+{
+	GENERATED_BODY()
+
+	// Add interface functions to this class. This is the class that will be inherited to implement this interface.
+public:
+	virtual TArray<TObjectPtr<AUnit>> GetSelectedUnits() const = 0;
+};
