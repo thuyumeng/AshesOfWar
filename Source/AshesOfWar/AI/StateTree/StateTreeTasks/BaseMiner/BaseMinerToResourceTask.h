7b2e57a0f9204ae9188555ed56120bf21569aa7b
yumeng <yumengch727727@gmail.com>
2025-04-10 08:33:06 +0800

    refs #week_4 remove startercontent

--- Source/AshesOfWar/AI/StateTree/StateTreeTasks/BaseMiner/BaseMinerToResourceTask.h
+++ Source/AshesOfWar/AI/StateTree/StateTreeTasks/BaseMiner/BaseMinerToResourceTask.h
@@ -31,6 +31,4 @@ private:
 	// the miner that is going to the resource
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Context", meta=(AllowPrivateAccess=true))
 	TObjectPtr<AUnitAIController> AIController;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Context", meta=(AllowPrivateAccess=true))
 };
